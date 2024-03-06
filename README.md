8e6e3c2e6420725c2de2ef1d5590934db3d40eca
vadapav_WSL_lab3_PC193 <ConfoundingVariables@github.com>
2024-03-06 13:18:30 +0530

    email validation

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Tasks:
 
-- [x] Add Tasks
+- [ ] Add Tasks
--- models/User.js
+++ models/User.js
@@ -3,44 +3,36 @@ const validator = require("validator");
 const bcrypt = require("bcrypt");
 
 const UserSchema = new mongoose.Schema({
-    email: {
-      type: String, // Define type for consistency
-      required: true, 
-      unique: true, // no duplicates
-      lowercase: true, // emails are not case sensitive
-      validate: (value) => {
-        if (!validator.isEmail(value)) {
-          throw new Error('Invalid email address.');
-        }
-      },
+  email: {
+    type: String, // Define type for consistency
+    required: true,
+    unique: true, // no duplicates
+    lowercase: true, // emails are not case sensitive
+    validate: (value) => {
+      if (!validator.isEmail(value)) {
+        throw new Error("Invalid email address.");
+      }
     },
-    username: { // Display name for profile**
-      type: String,
-      required: true,
-      unique: true
-    },
-    password: {
-      type: String,
-      required: true,
-      minlength: 8
-    },
-})
-
-// Hashing the password before saving it to the database
-userSchema.pre('save', async function(next) {
-  const user = this;
-  if (user.isModified('password')) {
-    user.password = await bcrypt.hash(user.password, 10);
-  }
-  next();
+  },
+  username: {
+    // Display name for profile**
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
 });
 
 //Middleware function to run before executing "save" query on DB
-UserSchema.pre('save', function (next) {
+UserSchema.pre("save", function (next) {
   const saltIterations = 10; // saltRounds -> saltIterations
   // Check if the password field has been modified before hashing it
-  if (this.modifiedPaths().includes('password')) {
-    // extra layer of security 
+  if (this.modifiedPaths().includes("password")) {
+    // extra layer of security
     bcrypt.genSalt(saltIterations, (err, salt) => {
       if (err) return next(err);
       bcrypt.hash(this.password, salt, (err, hash) => {
@@ -54,6 +46,24 @@ UserSchema.pre('save', function (next) {
   }
 });
 
+UserSchema.pre("save", async function (next) {
+  if (this.isNew) {
+    try {
+      const document = await User.findOne({
+        $or: [{ email: this.email }, { username: this.username }],
+      });
+      if (document)
+        return next(
+          new RequestError(
+            "A user with that email or username already exists.",
+            400
+          )
+        );
+    } catch (err) {
+      return next((err.statusCode = 400));
+    }
+  }
+});
 
-const User = mongoose.model('User', UserSchema);
+const User = mongoose.model("User", UserSchema);
 module.exports = User;
